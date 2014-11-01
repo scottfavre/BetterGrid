99b22193c8dd90bae96506ae3e2834560780eb09
Scott Favre <scottfavre@gmail.com>
2014-11-01 01:14:03 -0400

    added missing files

---
+++ BetterGrid/BoolToVisConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BetterGrid
+{
+    class BoolToVisConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            var input = (bool)value;
+            var param = Boolean.Parse((string)parameter);
+
+            if (input == param)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
